de2011d6cd1ebf199dda529c937d673f6610903e
Giwon Nam <gullivernam94@gmail.com>
2021-03-05 17:13:22 +0900

    210305_Controller 전문분별 후 service 호출하도록 수정

--- src/main/java/com/skcc/stock/service/StockService.java
+++ src/main/java/com/skcc/stock/service/StockService.java
@@ -1,8 +1,19 @@
 package com.skcc.stock.service;
 
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface StockService {
-	public String getStockInfo(HttpServletRequest request) throws Exception;
+import com.skcc.stock.dao.DocumentDao;
+import com.skcc.stock.dao.DocumentDaoFactory;
+import com.skcc.stock.option.DocumentType;
+
+@Service
+public class StockService {
+	@Autowired
+	private DocumentDaoFactory factory;
+	
+	public String getStockInfo(DocumentType type, String data) throws Exception {
+		DocumentDao dao = factory.findDocType(type);
+		return dao.fillDocument(data);
+	}
 }
-
